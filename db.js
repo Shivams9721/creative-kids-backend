5325a53dcc44c86aa42e6bb469fc4db53db38515
Shivam Singh <shivam.raj_cs21@gla.ac.in>
2026-03-12 17:10:23 +0530

    Fix pool initialization error in db.js

--- db.js
+++ db.js
@@ -1,11 +1,10 @@
-module.exports = pool;
 const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
-        rejectUnauthorized: false // <-- This is the magic line that fixes the error!
+        rejectUnauthorized: false
     }
 });
 
