b856a585037d06a910e2bdf2da0582979712f44f
Shivam Singh <shivam.raj_cs21@gla.ac.in>
2026-03-12 16:49:11 +0530

    Fix CORS and update product schema for AWS

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
+module.exports = pool;
 const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    require: true, // Forces the SSL connection to stay active
-    rejectUnauthorized: false
-  }
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false // <-- This is the magic line that fixes the error!
+    }
 });
 
 module.exports = pool;
